136bf60319adeb70d509a1271a2f59c8387a4dbb
theRealFlobo <florisbobvanelzelingen@gmail.com>
2016-10-14 11:41:54 +0200

    Wrote the code for the Node class

---
+++ MoviedApp/Shared Code/Node.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Shared_Code
+{
+    struct Message
+    {
+        public readonly int destinationId;
+        public readonly int originId;
+    }
+
+    class Node
+    {
+        private Queue<Message> inQueue = new Queue<Message>();
+
+        private static int NodeIdCounter = 1;
+        public readonly int Id = NodeIdCounter++;
+        public Action<NodeResponse, Message, Node> OnError = (r, m, n) => Console.WriteLine($"{r} on [{m}] by {n.Id}");
+
+        public enum NodeResponse
+        {
+            SUCCES,
+            DESTINATION_MISMATCH,
+            MESSAGE_FAULT
+        }
+
+
+        public Node(Func<Message, NodeResponse> messageHandler)
+        {
+            if (messageHandler == null)
+                throw new ArgumentNullException("messageHandler may not be NULL");
+
+            new Thread(() => MessageLoop(messageHandler)).Start();
+        }
+
+        public void AddMessage(Message m)
+        {
+            lock(inQueue)
+            {
+                inQueue.Enqueue(m);
+            }
+        }
+
+        private NodeResponse CheckValidity(Message message)
+        {
+            if (message.destinationId != Id)
+                return NodeResponse.DESTINATION_MISMATCH;
+            return NodeResponse.SUCCES;
+        }
+
+        private void MessageLoop(Func<Message, NodeResponse> messageHandler)
+        {
+            while (true)
+            {
+                lock (inQueue)
+                {
+                    int count = inQueue.Count();
+                    for (int i = 0; i < count; ++i)
+                    {
+                        Message m = inQueue.Dequeue();
+                        NodeResponse response;
+                        if ((response = CheckValidity(m)) != NodeResponse.SUCCES)
+                            OnError(response, m, this);
+                        else
+                            if ((response = messageHandler(m)) != NodeResponse.SUCCES)
+                                OnError(response, m, this);
+                    }
+                }
+                Thread.Yield();        
+            }
+        }
+    }
+}
