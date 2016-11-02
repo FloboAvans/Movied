69a1262684fde4b4a893a40530f1ac611dfeeb2b
theRealFlobo <florisbobvanelzelingen@gmail.com>
2016-11-02 11:44:25 +0100

    moved Node to Server

--- MoviedApp/Server/Node.cs
+++ MoviedApp/Server/Node.cs
@@ -4,8 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Shared_Code;
 
-namespace Shared_Code
+namespace Server
 {
     public class Node
     {
