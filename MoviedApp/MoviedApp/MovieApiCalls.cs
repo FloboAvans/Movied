c7f743cc1c5fe170cd991037402626b5189d2d33
marti <marti@DESKTOP-2TB6I0O>
2016-11-03 21:56:03 +0100

    started with api

---
+++ MoviedApp/MoviedApp/MovieApiCalls.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.TMDb;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MoviedApp
+{
+    class MovieApiCalls
+    {
+        private string apiKey;
+        private ServiceClient client;
+
+        public MovieApiCalls(string apiKey)
+        {
+            this.apiKey = apiKey;
+            client = new ServiceClient(apiKey);
+        }
+
+        static async Task Sample(CancellationToken cancellationToken)
+        {
+            
+        }
+}
