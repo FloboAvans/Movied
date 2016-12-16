11f28424a90e426cc8ec7b73fc64c2651a219334
theRealFlobo <florisbobvanelzelingen@gmail.com>
2016-12-16 11:08:32 +0100

    added the CheckIn struct

---
+++ MoviedApp/Shared Code/CheckIn.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared_Code
+{
+    struct CheckIn
+    {
+        // TODO possebly use specific layout and therefore consistand size
+        public int movieId;
+        public byte rating;
+        public string description;
+        // TODO add position
+        public long date, modDate;
+
+        public static long GetCurrentDate()
+        {
+            return DateTimeOffset.Now.To
+        }
+    }
+}
