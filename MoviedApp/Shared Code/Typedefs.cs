ea17a6e071c016bd48b17170ba7c29127c52d1e6
theRealFlobo <florisbobvanelzelingen@gmail.com>
2016-12-16 13:03:27 +0100

    completed the data & checkIn classes

---
+++ MoviedApp/Shared Code/Typedefs.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared_Code
+{
+    public struct MovieID
+    {
+        public int value;
+
+        public static implicit operator int(MovieID id)
+        {
+            return id.value;
+        }
+
+        public static implicit operator MovieID(int id)
+        {
+            return new MovieID {value = id};
+        }
+    }
+
+    public struct Date
+    {
+        public long value;
+
+        public static implicit operator long(Date date)
+        {
+            return date.value;
+        }
+
+        public static implicit operator Date(long date)
+        {
+            return new Date {value = date};
+        }
+    }
+
+    public struct UserID
+    {
+        public int value;
+
+        public static implicit operator int(UserID id)
+        {
+            return id.value;
+        }
+
+        public static implicit operator UserID(int id)
+        {
+            return new UserID {value = id};
+        }
+    }
+}
