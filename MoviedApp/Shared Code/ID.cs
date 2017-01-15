7a1de9eb4e178354f9ba3e4f1d6b52b33e08eee7
FloboAvans <fbelzeli@student.avans.nl>
2017-01-15 23:18:42 +0100

    finished merge

--- MoviedApp/Shared Code/ID.cs
+++ MoviedApp/Shared Code/ID.cs
@@ -28,15 +28,9 @@ namespace Shared_Code
 
         public JToken Serialize()
         {
-<<<<<<< HEAD
-            // TODO finsish this
-            //JToken
-            return null;
-=======
             JObject jObject = new JObject();
             jObject.Add("mID", mID);
             return jObject;
->>>>>>> 9f41f266da7a0c13a4dfdb41cf986d7b5337a042
         }
 
         public ID<T> super
