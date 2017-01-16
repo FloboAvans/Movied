3b4c3020c4ea8618056f9d3e66635ec048009b56
FloboAvans <fbelzeli@student.avans.nl>
2017-01-16 01:09:27 +0100

    started on login code

--- MoviedApp/DebugApp/Program.cs
+++ MoviedApp/DebugApp/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Shared_Code;
+using Shared_Code.Client_Side;
 
 namespace DebugApp
 {
@@ -10,7 +12,53 @@ namespace DebugApp
     {
         static void Main(string[] args)
         {
+            ServerHandler.instance.OnHandshakeComplete += LoginHandler;
+        }
 
+        private static void LoginHandler()
+        {
+            string mode;
+            do
+            {
+                Console.WriteLine("\"login\" or \"create\"");
+                mode = Console.ReadLine();
+            } while (mode != "login" && mode != "create");
+            if (mode == "login") Login1();
+            else Create();
+        }
+
+        private static void Login1()
+        {
+                Console.Write("username: ");
+                string username = Console.ReadLine();
+
+                ServerHandler.instance.SendMessage(new Message
+                {
+                    destinationID = ServerHandler.instance.serverNodeID,
+                    senderID = ServerHandler.instance.clientID,
+                    traceNumber = TraceID.GenerateTraceID(),
+                    type = Message.Type.ClientServer.Login.saltRequest,
+                    isResponse = false,
+                    message = new {username = username},
+                    succes = true
+                }, m1 =>
+                {
+                    if (m1.succes = false)
+                    {
+                        Console.WriteLine("username does not exist");
+                        Login1();
+                    }
+                    else
+                    {
+                        byte[] saltBytes = Convert.FromBase64String((string) m1.message.salt);
+                        Console.WriteLine("salt extracted");
+                    }
+                });
+        }
+
+        private static void Create()
+        {
+            
         }
     }
 }
