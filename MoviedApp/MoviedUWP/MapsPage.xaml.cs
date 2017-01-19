ad21af92fc8e95ae916bb730a5763f54791092cb
marti <marti@DESKTOP-2TB6I0O>
2017-01-19 23:52:18 +0100

    changes in map

--- MoviedApp/MoviedUWP/MapsPage.xaml.cs
+++ MoviedApp/MoviedUWP/MapsPage.xaml.cs
@@ -39,30 +39,22 @@ namespace MoviedUWP
 
         private async void MainPage_Loaded(object sender, RoutedEventArgs e)
         {
-            // Map Token for testing purpose,   
-            // otherwise you'll get an alart message in Map Control  
-            MyMap.MapServiceToken = "abcdef-abcdefghijklmno";
-
             geolocator = new Geolocator();
             geolocator.DesiredAccuracyInMeters = 50;
-
             try
-            {
-                // Getting Current Location  
+            { 
                 Geoposition geoposition = await geolocator.GetGeopositionAsync(
                     maximumAge: TimeSpan.FromMinutes(5),
                     timeout: TimeSpan.FromSeconds(10));
 
                 MapIcon mapIcon = new MapIcon();
-                // Locate your MapIcon  
+                 
                 mapIcon.Image = RandomAccessStreamReference.CreateFromUri(new Uri("ms-appx:///Assets/my-position.png"));
-                // Show above the MapIcon  
-                mapIcon.Title = "Current Location";
-                // Setting up MapIcon location  
+
+                //mapIcon.Title = "Current Location";
+
                 mapIcon.Location = new Geopoint(new BasicGeoposition()
-                {
-                    //Latitude = geoposition.Coordinate.Latitude, [Don't use]  
-                    //Longitude = geoposition.Coordinate.Longitude [Don't use]  
+                { 
                     Latitude = geoposition.Coordinate.Point.Position.Latitude,
                     Longitude = geoposition.Coordinate.Point.Position.Longitude
                 });
@@ -72,35 +64,20 @@ namespace MoviedUWP
                 // Showing in the Map  
                 await MyMap.TrySetViewAsync(mapIcon.Location, 18D, 0, 0, MapAnimationKind.Bow);
 
-                // Disable the ProgreesBar  
+                 
                 progressBar.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
-                // Set the Zoom Level of the Slider Control  
-                mySlider.Value = MyMap.ZoomLevel;
+                border.Visibility = Visibility.Collapsed;
             }
             catch (UnauthorizedAccessException)
             {
                 MessageBox("Location service is turned off!");
             }
-            //base.OnNavigatedTo(e);
         }
-
-        // Slider Control  
-        private void ZoomValueChanged(object sender, RangeBaseValueChangedEventArgs e)
-        {
-            if (MyMap != null)
-                MyMap.ZoomLevel = e.NewValue;
-        }
-
-        private void MyMap_ZoomLevelChanged(MapControl sender, object args)
-        {
-            if (MyMap != null)
-                mySlider.Value = sender.ZoomLevel;
-        }
-
-        // Locate Me Bottom App Bar  
+        
         private async void LocateMe_Click(object sender, RoutedEventArgs e)
         {
             progressBar.Visibility = Windows.UI.Xaml.Visibility.Visible;
+            border.Visibility = Visibility;
             geolocator = new Geolocator();
             geolocator.DesiredAccuracyInMeters = 50;
 
@@ -110,16 +87,14 @@ namespace MoviedUWP
                     maximumAge: TimeSpan.FromMinutes(5),
                     timeout: TimeSpan.FromSeconds(10));
                 await MyMap.TrySetViewAsync(geoposition.Coordinate.Point, 18D);
-                mySlider.Value = MyMap.ZoomLevel;
                 progressBar.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+                border.Visibility = Visibility.Collapsed;
             }
             catch (UnauthorizedAccessException)
             {
                 MessageBox("Location service is turned off!");
             }
         }
-
-        // Custom Message Dialog Box  
         private async void MessageBox(string message)
         {
             var dialog = new MessageDialog(message.ToString());
--- MoviedApp/MoviedUWP/Properties/AssemblyInfo.cs
+++ MoviedApp/MoviedUWP/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MoviedUWP")]
+[assembly: AssemblyTitle("Movied")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MoviedUWP")]
+[assembly: AssemblyProduct("Movied")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
