97b432cf41f525132f0ce1189b425c12d6fa2f42
theRealFlobo <florisbobvanelzelingen@gmail.com>
2017-01-20 00:32:12 +0100

    work on checkIn & watchlist server side

---
+++ MoviedApp/Server/DataNode.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shared_Code;
+using Shared_Code_Portable;
+
+namespace Server
+{
+    class DataNode : Node
+    {
+        class UserEntry
+        {
+            public List<CheckIn> checkIns = new List<CheckIn>();
+            //public List<Tuple<MovieID, Date, UserID>> recomendations = new List<Tuple<MovieID, Date, UserID>>();
+            public List<Tuple<MovieID, Date>> wishlist = new List<Tuple<MovieID, Date>>();
+        }
+
+        public static DataNode instance = new DataNode();
+
+        private DataNode() : base(MessageHandler, Constants.Server.DATA_NODE_ADDRESS)
+        {
+            
+        }
+
+        private Dictionary<UserID, UserEntry> userEntries = new Dictionary<UserID, UserEntry>();
+
+        private static ID<NodeResponse> MessageHandler(Node node, Message message)
+        {
+            if (node.GetType() != typeof(DataNode))
+                return Node.NodeResponse.PreCheck.nodeMismatch;
+
+            if (message.type.isa(Message.Type.ClientServer.User.checkIn) == false)
+                return NodeResponse.messageTypeMismatch;
+
+            UserID userid = (int)message.message.userid;
+
+            if (message.type == Message.Type.ClientServer.User.CheckIn.create)
+            {
+                bool exists = instance.userEntries.ContainsKey(userid);
+                if (exists)
+                {
+                    bool hasCheckedIn = (instance.userEntries
+                                .Single(pair => pair.Key == userid)
+                        ).Value.checkIns
+                        .Contains(message.message.checkIn);
+
+
+                }
+            }
+
+            return default(ID<NodeResponse>);
+        }
+    }
+
+    
+}
--- MoviedApp/Shared Code Portable/CheckIn.cs
+++ MoviedApp/Shared Code Portable/CheckIn.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 
 namespace Shared_Code
 {
@@ -13,6 +14,7 @@ namespace Shared_Code
 
     public struct CheckIn
     {
+        /*
         [Flags]
         public enum ContentE : byte
         {
@@ -22,27 +24,44 @@ namespace Shared_Code
             Date = 8,
             ModDate = 16
         }
-
+        */
         // TODO possebly use specific layout and therefore consistand size
-        public ContentE Content;
+        //public ContentE Content;
         public int movieId;
-        public byte rating;
+        public byte? rating;
         public string description;
-        public Coordinate Position;
-        public long date, modDate;
+        //public Coordinate Position;
+        //public long? date, modDate;
 
         public static long GetCurrentDate()
         {
             return (int)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
         }
 
+        public JObject Serialize()
+        {
+            JObject jObject = new JObject();
+            jObject.Add("movieId", movieId);
+            
+            return jObject;
+        }
+
+        public CheckIn(JObject jObject)
+        {
+            JToken _out;
+            movieId = (int) jObject["movieId"];
+            rating = jObject.TryGetValue("rating", out _out) ? (byte?) _out : null;
+            description = jObject.TryGetValue("description", out _out) ? (string)_out : null;
+        }
+
         private void test()
         {
             CheckIn checkIn = new CheckIn
             {
-                Content = ContentE.Description | ContentE.Date,
+                movieId = 123,
                 description = "super awesome",
-                date = CheckIn.GetCurrentDate()
+                rating = 10
+                //date = GetCurrentDate()
             };
         }
     }
